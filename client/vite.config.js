2afe824c119ef71d15567bd7ff5b42ee7ec7bb9f
SugonChenxu <chenxu@hpm.local>
2026-07-09 14:42:29 +0800

    fix: disable React Fast Refresh to eliminate $RefreshSig$ error

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ fastRefresh: false })],
   server: { port: 5173, proxy: { "/api": "http://localhost:3001" } },
 });
